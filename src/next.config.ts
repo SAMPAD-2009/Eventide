ef9bda7dc98df1710b3876c1d6f0e6714f301f32
Sampad Chakrabarty <sampadchakrabarty81@gmail.com>
2025-09-22 10:58:18 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

---
+++ src/next.config.ts
@@ -0,0 +1,52 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.britannica.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.weatherapi.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    // Keep other experimental features here if any
+  },
+  allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+};
+
+export default nextConfig;
