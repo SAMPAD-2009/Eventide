4ad091dc82f04131e503bb9e06f008cde3e48306
Sampad Chakrabarty <sampadchakrabarty81@gmail.com>
2025-10-11 14:53:08 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/todo/TodoSidebar.tsx
+++ src/components/todo/TodoSidebar.tsx
@@ -21,6 +21,7 @@ import {
     AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
+    AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
