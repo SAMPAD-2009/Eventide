52815486eb354ec554fe30f6b4db60bbe6f13de5
Sampad Chakrabarty <sampadchakrabarty81@gmail.com>
2025-10-18 12:28:34 +0000

    dont show the sidebar on the note editor page

--- src/app/notes/[notebookId]/[noteId]/page.tsx
+++ src/app/notes/[notebookId]/[noteId]/page.tsx
@@ -5,15 +5,9 @@ import { useNotes } from '@/context/NoteContext';
 import { useParams, useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { Note, Notebook } from '@/lib/types';
-import { NoteSidebar } from '@/components/notes/NoteSidebar';
 import { NoteBreadcrumbs } from '@/components/notes/NoteBreadcrumbs';
 import { TiptapEditor } from '@/components/notes/TiptapEditor';
 import { Skeleton } from '@/components/ui/skeleton';
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
 
 export default function NoteEditorPage() {
   const { notebookId, noteId } = useParams();
@@ -46,15 +40,12 @@ export default function NoteEditorPage() {
   };
 
   const renderSkeleton = () => (
-    <div className="flex h-[calc(100vh-4rem)]">
-      <Skeleton className="w-72 hidden md:block border-r" />
-      <div className="flex-1 flex flex-col">
-        <header className="p-4 border-b">
-          <Skeleton className="h-6 w-1/2" />
-        </header>
-        <div className="flex-1 p-4">
-          <Skeleton className="h-full w-full" />
-        </div>
+    <div className="flex flex-col h-[calc(100vh-4rem)]">
+      <header className="p-4 border-b">
+        <Skeleton className="h-6 w-1/2" />
+      </header>
+      <div className="flex-1 p-4">
+        <Skeleton className="h-full w-full" />
       </div>
     </div>
   );
@@ -64,32 +55,18 @@ export default function NoteEditorPage() {
   }
 
   return (
-      <ResizablePanelGroup
-        direction="horizontal"
-        className="h-[calc(100vh-4rem)] w-full rounded-none border-none"
-      >
-        <ResizablePanel defaultSize={25} minSize={15} collapsible={true} collapsedSize={4}>
-           <NoteSidebar
-            currentNotebookId={notebook.notebook_id}
-            currentNoteId={note.note_id}
-          />
-        </ResizablePanel>
-        <ResizableHandle withHandle />
-        <ResizablePanel defaultSize={75}>
-            <main className="flex-1 flex flex-col h-full">
-                <header className="p-4 border-b">
-                <NoteBreadcrumbs
-                    notebook={notebook}
-                    note={note}
-                />
-                </header>
-                <TiptapEditor
-                  note={note}
-                  onSave={handleSave}
-                  isSaving={isSaving}
-                />
-            </main>
-        </ResizablePanel>
-    </ResizablePanelGroup>
+    <main className="flex-1 flex flex-col h-[calc(100vh-4rem)]">
+        <header className="p-4 border-b">
+        <NoteBreadcrumbs
+            notebook={notebook}
+            note={note}
+        />
+        </header>
+        <TiptapEditor
+          note={note}
+          onSave={handleSave}
+          isSaving={isSaving}
+        />
+    </main>
   );
 }
